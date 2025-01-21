7649a49420732c2ece24e8f40e23ebf7bbe95e44
naoures <124105771+NaouresBzeouich@users.noreply.github.com>
2025-01-21 01:49:09 +0100

    create Blog , profile , home
    I juste create the componen so I can make the layout a little clear , sorry for doing that without your permission :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { LogInComponent } from './log-in/log-in.component';
+import { ProfileComponent } from './profile/profile.component';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     SignInComponent,
-    LogInComponent
+    LogInComponent,
+    ProfileComponent,
+    HomeComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
