b7ce9d44e0595934140222795c4256ea24e34826
naoures <124105771+NaouresBzeouich@users.noreply.github.com>
2025-01-21 01:21:54 +0100

    create component NavBar , SignIn , LogIn

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SignInComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
