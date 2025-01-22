6e8b8e9729be278196445b673b6de3ba1eb3727e
naoures <124105771+NaouresBzeouich@users.noreply.github.com>
2025-01-22 01:30:51 +0100

    make observable to the routes so a specific list of routes will not contain the navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from './components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
+import { Router, Event, NavigationEnd } from '@angular/router';
+import { Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
+import {FooterComponent} from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, CommonModule , FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true,
 })
 export class AppComponent {
   title = 'Blog-Website';
+
+  showNavbar$: Observable<boolean>;
+
+  constructor(private router: Router) {
+    this.showNavbar$ = this.router.events.pipe(
+      filter((event: Event): event is NavigationEnd => event instanceof NavigationEnd),
+      map((event: NavigationEnd) => {
+        const hideNavbarRoutes = ['/signin', '/signup'];
+        return !hideNavbarRoutes.some(route => event.url.includes(route));
+      })
+    );
+  }
 }
