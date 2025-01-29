a7f42346242fa8568e31e7938bb92bc25ae37790
naoures <124105771+NaouresBzeouich@users.noreply.github.com>
2025-01-29 15:16:10 +0100

    lazy loading routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,5 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
-import {NotificationComponent} from './components/notification/notification.component';
-import {SigninComponent} from './auth/signin/signin.component';
-import {SignupComponent} from './auth/signup/signup.component';
 import { BlogComponent } from './components/blog/blog.component';
 import {ProfileComponent} from './profile/profile/profile.component';
 import {CreateBlogComponent} from './components/create-blog/create-blog.component';
@@ -10,17 +7,31 @@ import {AuthGuard} from './auth/guard/guard.guard';
 import {EditBlogComponent} from './components/edit-blog/edit-blog.component';
 export const routes: Routes = [
   { path: '', component: HomeComponent }, // Default route
-  { path: 'signin', component: SigninComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'profile/:id', component: ProfileComponent  },
-  { path: 'notification', component: NotificationComponent },
+  { path: 'signin',
+    loadComponent: ()=> import('./auth/signin/signin.component').then(c=>c.SigninComponent) ,
+  },
+  { path: 'signup',
+    loadComponent : ()=> import('./auth/signup/signup.component').then(c=>c.SignupComponent) ,
+  },
+  { path: 'profile/:id',
+    loadComponent : ()=> import('./profile/profile/profile.component').then(c=>c.ProfileComponent) ,
+  },
   {path : 'blog',
     children: [
-      { path: 'new', component: CreateBlogComponent , canActivate: [AuthGuard] },
-      { path: 'edit/:id', component:  EditBlogComponent , canActivate: [AuthGuard] },
-      { path: ':id', component: BlogComponent },
+      { path: 'new',
+        loadComponent : ()=> import('./components/create-blog/create-blog.component').then(c=>c.CreateBlogComponent) ,
+        canActivate: [AuthGuard]
+      },
+      { path: 'edit/:id',
+        loadComponent : ()=> import('./components/edit-blog/edit-blog.component').then(c=>c.EditBlogComponent) ,
+        canActivate: [AuthGuard]
+      },
+      { path: ':id',
+        loadComponent : ()=> import('./components/blog/blog.component').then(c=>c.BlogComponent) ,
+      },
     ]
   },
 
   { path: '**', redirectTo: '' }
+
 ];
